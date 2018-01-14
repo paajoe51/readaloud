b35fffe3f730e900b712fbb671dcc4d9b5568567
Michael Abordzi <paajoe51@gmail.com>
2018-01-14 11:53:09 +0000

    changed activated the read button, and some other cool stuff

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  readtext:string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private tts: TextToSpeech) {
 
-  }
+    }
+
+    read(){
+      this.tts.speak("In the begining was the word, and the word was God, and the word was with God.")
+        .then(() => alert("done reading"))
+          
+
+        .catch((reason: any) => console.log(reason))
+    }
 
 }
